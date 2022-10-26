fd0782fedbcab78e77b3d6b0c9d3ac72d1d1f384
Elliott Landsborough <elliott.landsborough@gmail.com>
2022-10-26 12:59:05 +0100

    Test command output
    (cherry picked from commit eb4f5c82fa9a8964597bcdbca779951fe2c2ab1a)

--- tests/Commands/ParseCronTest.php
+++ tests/Commands/ParseCronTest.php
@@ -7,18 +7,53 @@ namespace ElliottLandsborough\PhpTerminalApp\Tests\Services;
 use ElliottLandsborough\PhpTerminalApp\Commands\ParseCron;
 use Exception;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class ParseCronTest extends TestCase
 {
-    protected $cron;
+    protected $commandTester;
 
-    // runs per test
-    public function setUp(): void
+    protected function setUp(): void
     {
-        $this->cron = new Cron();
+        $application = new Application();
+        $application->add(new ParseCron());
+        $command = $application->find('parseCron');
+        $this->commandTester = new CommandTester($command);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->commandTester = null;
+    }
+
+    public function testBadString()
+    {
+        $this->expectException(Exception::class);
+        $this->commandTester->execute(['cron_string' => 'bad string']);
     }
 
     public function testCommand()
     {
+        $this->commandTester->execute(
+            [
+                'cron_string' => '*/30 0,1,2,3 1-2 2-3 3-4 /path/executable --argument'
+            ]
+        );
+
+        $output = trim($this->commandTester->getDisplay());
+
+        $expected = <<<'EOD'
++--------------+-----------------------------+
+| Minute       | 0 30                        |
+| Hour         | 0 1 2 3                     |
+| Day of month | 1 2                         |
+| Month        | 2 3                         |
+| Day of week  | 3 4                         |
+| Command      | /path/executable --argument |
++--------------+-----------------------------+
+EOD;
+
+        $this->assertEquals($output, $expected);
     }
 }
